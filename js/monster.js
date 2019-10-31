45bf8857787ae2b45045d25656d9fe6dac2a5096
Juan Grajales <grajales.juanfe11@gmail.com>
2019-10-31 15:35:14 -0400

    integrate game class

--- js/monster.js
+++ js/monster.js
@@ -1,12 +1,25 @@
-class Monster{
-    contstructor(x,y,width,height){
+class Monster {
+    /**
+     * 
+     * @param {x axis position on canvas} x 
+     * @param {y axis position on canvas} y 
+     * @param {width of player} width 
+     * @param {height of player} height 
+     * @param {default monster type is a snake } monsterType 
+     */
+    constructor(x,y,width,height, monsterType){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+
+        // default character is a snake
+        const snakeImg = new Image();
+        snakeImg.src = "../images/enemy-snake.png";
+        this.monsterType = snakeImg;
     }
 
-    moveMonster(){
+    moveMonster() {
         setInterval(()=>{
             this.x =Math.floor(Math.random()*700)+1;
             setInterval(()=>{
@@ -18,11 +31,8 @@ class Monster{
             },500);
 
         },10000)
-            
-        
-        
     }
-    
 }
+
 
 
