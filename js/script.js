1134a055a8c297a33bc201790e0b1960425e6bc7
jpcervera6 <jpcervera6@gmail.com>
2019-10-31 14:26:52 -0400

    Fixed

--- js/script.js
+++ js/script.js
@@ -5,57 +5,45 @@
  */
 
 // create the canvas that you will be working, assing it to ctx
+// create the canvas that you will be working, assing it to ctx
 const ctx = document.getElementById('rock-board').getContext('2d');
-
-// variables that hold the canvas width and height in case the canvas size changes
-let canvasWidth = document.querySelector("#rock-board").width;
-let canvasHeight = document.querySelector("#rock-board").height;
-console.log(canvasHeight);
-console.log(canvasWidth);
-
 let frames = 0;
-let miner = new Player(500,500,50,50);
-const playerImg = new Image();
+let miner = new Player(0,0,50,50);
+let playerImg = new Image();
 playerImg.src = '../images/gold-miner.png';
-
-let snake = new Monster(300,300,50,40);
-const snakeImg = new Image();
+let snake = new Monster(100,100,50,40)
+let snakeImg = new Image();
 snakeImg.src = '../images/enemy-snake.png';
-
 function drawImages() {
-    console.log('miner');
-    //ctx.clearRect(0, 0, 400, 800);
-    ctx.drawImage(playerImg, miner.x, miner.y, miner.width, miner.height);
-    ctx.drawImage(snakeImg,snake.x,snake.y,snake.width, snake.height);
-    console.log('snake');
-  }
-  
-
+   ctx.drawImage(playerImg, miner.x, miner.y, miner.width, miner.height);
+   ctx.drawImage(snakeImg, snake.x, snake.y, snake.width, snake.height);
+ }
 function mainLoop(){
-    frames++;
-    ctx.clearRect(0,0,700,700);
-    drawImages();
-    ctx.clearRect(0,0,canvasWidth,canvasHeight);
-    requestAnimationFrame(mainLoop);
+   frames++;
+   ctx.clearRect(0,0,700,700);
+   drawImages();
+   requestAnimationFrame(mainLoop);
 }
-
 // controls how fast the character moves
 let characterSpeed = 30;
-
 // depending on the direction the user presses move in that direction
 document.onkeydown = function(e){
-  if(e.key === "ArrowUp"){
-    miner.movePlayer(miner.x,miner.y - characterSpeed);
-  }
-  if(e.key === "ArrowDown"){
-    miner.movePlayer(miner.x,miner.y + characterSpeed);
-  }
-  if(e.key === "ArrowLeft"){
-    miner.movePlayer(miner.x - characterSpeed,miner.y);
-  }
-  if(e.key === "ArrowRight"){
-    miner.movePlayer(miner.x + characterSpeed,miner.y);
-  }
+ if(e.key === "ArrowUp"){
+   miner.movePlayer(miner.x,miner.y - characterSpeed);
+ }
+ if(e.key === "ArrowDown"){
+   miner.movePlayer(miner.x,miner.y + characterSpeed);
+ }
+ if(e.key === "ArrowLeft"){
+   miner.movePlayer(miner.x - characterSpeed,miner.y);
+ }
+ if(e.key === "ArrowRight"){
+   console.log(playerImg)
+   miner.movePlayer(miner.x + characterSpeed,miner.y);
+ }
 }
-
 mainLoop();
+
+
+
+
