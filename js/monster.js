5f4909541c5c2f1eb15f350e19eb3703911f77bc
jpcervera6 <jpcervera6@gmail.com>
2019-10-31 12:02:18 -0400

    Added moveMonster function

--- js/monster.js
+++ js/monster.js
@@ -3,10 +3,25 @@ class Monster{
         this.x = x;
         this.y = y;
         this.width = width;
-        this.height = heigth;
+        this.height = height;
     }
 
+    moveMonster(){
+        setInterval(()=>{
+            this.x =Math.floor(Math.random()*)+1;
+            setInterval(()=>{
+                this.x +=; 
+            },500);
+            this.y = Math.floor(Math.random()*)+1;
+            setInterval(()=>{
+                this.y +=; 
+            },500);
 
+        },10000)
+            
+        
+        
+    }
     
 }
 
