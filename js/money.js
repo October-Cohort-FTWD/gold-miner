2e2609a9fcb18d22ab6665e3728141eff7a2c81f
jpcervera6 <jpcervera6@gmail.com>
2019-10-31 15:32:21 -0400

    Commit

---
+++ js/money.js
@@ -0,0 +1,12 @@
+class Money {
+    constructor(x,y,width,height,type){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.type = type;
+    }
+
+
+}
+
